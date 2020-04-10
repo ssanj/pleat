7f4cf421c2ab636d3bfba8eae30ef27a4d89b4ec
sanjiv sahayam <sanjsmailbox@gmail.com>
2020-04-10 19:01:24 +1000

    Simplify tests

--- test/Format/GitBranchSpec.hs
+++ test/Format/GitBranchSpec.hs
@@ -1,73 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Format.GitBranchSpec (test_all) where
+module Format.GitBranchSpec where
 
 import qualified Parser.GitParser as Git
 
 import Test.Tasty                      (TestTree)
 import Test.Tasty                      (testGroup)
-import Test.Tasty.HUnit                (assertFailure)
-import Test.Tasty.HUnit                (testCase)
-import Test.Tasty.HUnit                ((@?=))
+import Test.Tasty.HUnit                (assertFailure, testCase, (@?=), Assertion)
 import Data.Functor.Identity           (Identity)
 import Text.Parsec                     (parse)
 
 import Format.GitBranch
 
-test_all :: TestTree
-test_all = 
-  testGroup 
-    "GitParser" 
-    [
-       processGitBranchWithEmptyInput
-    ,  processGitBranchWithNoInput
-    ,  processGitBranchWithoutActiveBranch
-    ,  processGitBranchWithRemoteBranch
-    ,  processGitBranchWithLocalBranch
-    ,  processGitBranchWithUnparsableInput
-    ]
+unit_processGitBranchWithEmptyInput :: Assertion
+unit_processGitBranchWithEmptyInput = processGitBranch (Just []) @?= (Just InitialCommit)
 
-processGitBranchWithEmptyInput :: TestTree
-processGitBranchWithEmptyInput = 
-  withTestCase "processGitBranchWithEmptyInput" $ 
-    processGitBranch (Just []) @?= (Just InitialCommit)
+unit_processGitBranchWithNoInput :: Assertion
+unit_processGitBranchWithNoInput = processGitBranch Nothing @?= Nothing
 
-processGitBranchWithNoInput :: TestTree
-processGitBranchWithNoInput = 
-  withTestCase "processGitBranchWithNoInput" $ 
-    processGitBranch Nothing @?= Nothing
+unit_processGitBranchWithRemoteBranch :: Assertion
+unit_processGitBranchWithRemoteBranch = 
+  processGitBranch (Just [
+                            "2                          3d5ae73 Release 0.0.9-b01"
+                          , "add-macro-support          4cf335e [origin/add-macro-support] Choose Exception syntax"
+                          , "alternate-exception-syntax d7a99e8 [origin/alternate-exception-syntax] Alternate Exception Syntax"
+                          , "* do-something-else                     3d5ae73 [moon/do-something-else] Release 0.0.9-b01"
+                          , "composite-assertions       6757633 [origin/composite-assertions] Add travis"
+                         ]) @?= Just (Remote (Git.RemoteBranch "moon" "do-something-else"))
 
-processGitBranchWithoutActiveBranch :: TestTree
-processGitBranchWithoutActiveBranch = 
-  withTestCase "processGitBranchWithoutActiveBranch" $ 
-    processGitBranch (Just [
-                              "2                          3d5ae73 Release 0.0.9-b01"
-                            , "add-macro-support          4cf335e [origin/add-macro-support] Choose Exception syntax"
-                            , "alternate-exception-syntax d7a99e8 [origin/alternate-exception-syntax] Alternate Exception Syntax"
-                            , "composite-assertions       6757633 [origin/composite-assertions] Add travis"
-                           ]) @?= Nothing
+unit_processGitBranchWithLocalBranch :: Assertion
+unit_processGitBranchWithLocalBranch = 
+  processGitBranch (Just ["* slave 99d80d9f Merge pull request #1827 from scala-steward/update/sbt-1.3.9"]) 
+    @?= Just (Local (Git.LocalBranch "slave"))
 
-processGitBranchWithRemoteBranch :: TestTree
-processGitBranchWithRemoteBranch = 
-  withTestCase "processGitBranchWithRemoteBranch" $ 
-    processGitBranch (Just [
-                              "2                          3d5ae73 Release 0.0.9-b01"
-                            , "add-macro-support          4cf335e [origin/add-macro-support] Choose Exception syntax"
-                            , "alternate-exception-syntax d7a99e8 [origin/alternate-exception-syntax] Alternate Exception Syntax"
-                            , "* do-something-else                     3d5ae73 [moon/do-something-else] Release 0.0.9-b01"
-                            , "composite-assertions       6757633 [origin/composite-assertions] Add travis"
-                           ]) @?= Just (Remote (Git.RemoteBranch "moon" "do-something-else"))
-
-processGitBranchWithLocalBranch :: TestTree
-processGitBranchWithLocalBranch = 
-  withTestCase "processGitBranchWithLocalBranch" $ 
-    processGitBranch (Just ["* slave 99d80d9f Merge pull request #1827 from scala-steward/update/sbt-1.3.9"]) 
-      @?= Just (Local (Git.LocalBranch "slave"))
-
-processGitBranchWithUnparsableInput :: TestTree
-processGitBranchWithUnparsableInput = 
-  withTestCase "processGitBranchWithUnparsableInput" $ 
-    processGitBranch (Just [""]) @?= Nothing
-
-withTestCase :: String -> IO () -> TestTree
-withTestCase name = testCase name
+unit_processGitBranchWithUnparsableInput :: Assertion
+unit_processGitBranchWithUnparsableInput = processGitBranch (Just [""]) @?= Nothing
--- test/Parser/GitParserSpec.hs
+++ test/Parser/GitParserSpec.hs
@@ -1,67 +1,59 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Parser.GitParserSpec (test_all) where
+module Parser.GitParserSpec where
 
-import Test.Tasty                      (TestTree)
-import Test.Tasty                      (testGroup)
-import Test.Tasty.HUnit                (assertFailure)
-import Test.Tasty.HUnit                (testCase)
-import Test.Tasty.HUnit                ((@?=))
+import Test.Tasty                      (TestTree, testGroup)
+import Test.Tasty.HUnit                (Assertion, assertFailure, testCase, (@?=))
 import Data.Functor.Identity           (Identity)
 import Parser.GitParser                (LocalBranch(..), RemoteBranch(..), localBranch, remoteBranch)
 import Text.Parsec                     (parse)
 
-test_all :: TestTree
-test_all = 
-  testGroup 
-    "GitParser" 
-    [
-       testWithLocalBranch     "* master b93b0b7 More WIP"
-    ,  testWithoutLocalBranch  "Merge pull request #1715"
-    ,  testWithRemoteBranch    "* master b7fd5fb0 [origin/master] Merge pull request #1715 from jneira/fix-install-hoogle"
-    ,  testWithoutRemoteBranch "* master b93b0b7 More WIP"
-    ]
+-- test_all :: TestTree
+-- test_all = 
+--   testGroup 
+--     "GitParser" 
+--     [
+--        testWithLocalBranch     "* master b93b0b7 More WIP"
+--     ,  testWithoutLocalBranch  "Merge pull request #1715"
+--     ,  testWithRemoteBranch    "* master b7fd5fb0 [origin/master] Merge pull request #1715 from jneira/fix-install-hoogle"
+--     ,  testWithoutRemoteBranch "* master b93b0b7 More WIP"
+--     ]
 
-testWithLocalBranch :: String -> TestTree
-testWithLocalBranch input = 
-  let parseResult = parse localBranch "" input
-      result      = either (assertFailure . show) (\lbranch -> 
-                                                    case lbranch of 
-                                                      Just (LocalBranch branchName) -> branchName @?= "master"
-                                                      Nothing                       -> assertFailure "Local branch not found"
-                                                  ) parseResult
-  in testCase "testWithLocalBranch" result
+unit_parseWithLocalBranch :: Assertion
+unit_parseWithLocalBranch =
+  let parseResult = parse localBranch "" "* master b93b0b7 More WIP" in
+  either (assertFailure . show) (\lbranch -> 
+                                    case lbranch of 
+                                      Just (LocalBranch branchName) -> branchName @?= "master"
+                                      Nothing                       -> assertFailure "Local branch not found"
+                                ) parseResult
 
-testWithoutLocalBranch :: String -> TestTree
-testWithoutLocalBranch input = 
-  let parseResult = parse localBranch "" input
-      result      = either (assertFailure . show) (\lbranch -> 
-                                                    case lbranch of 
-                                                      Just (LocalBranch branchName) -> assertFailure $ "expected no remote branch but got: " <> branchName
-                                                      Nothing                       -> pure ()
-                                                  ) parseResult
-  in testCase "testWithoutLocalBranch" result
+unit_parseWithoutActiveBranch :: Assertion
+unit_parseWithoutActiveBranch = 
+  let parseResult = parse localBranch "" "Merge pull request #1715" in
+  either (assertFailure . show) (\lbranch -> 
+                                    case lbranch of 
+                                      Just (LocalBranch branchName) -> assertFailure $ "expected no remote branch but got: " <> branchName
+                                      Nothing                       -> pure ()
+                                ) parseResult
 
-testWithRemoteBranch :: String -> TestTree
-testWithRemoteBranch input = 
-  let parseResult = parse remoteBranch "" input
-      result      = either (assertFailure . show) (\rBranch -> 
-                                                    case rBranch of 
-                                                      Just (RemoteBranch remote branchName) -> 
-                                                        do 
-                                                          remote     @?= "origin" 
-                                                          branchName @?= "master"
-                                                      Nothing                        -> assertFailure "Remote branch not found"
-                                                  ) parseResult
-  in testCase "testWithRemoteBranch" result
+unit_parseWithRemoteBranch :: Assertion
+unit_parseWithRemoteBranch = 
+  let parseResult = parse remoteBranch "" "* master b7fd5fb0 [origin/master] Merge pull request #1715 from jneira/fix-install-hoogle" in
+  either (assertFailure . show) (\rBranch -> 
+                                    case rBranch of 
+                                      Just (RemoteBranch remote branchName) -> 
+                                        do 
+                                          remote     @?= "origin" 
+                                          branchName @?= "master"
+                                      Nothing                        -> assertFailure "Remote branch not found"
+                                ) parseResult
 
-testWithoutRemoteBranch :: String -> TestTree
-testWithoutRemoteBranch input = 
-  let parseResult = parse remoteBranch "" input
-      result      = either (assertFailure . show) (\rBranch -> 
-                                                    case rBranch of 
-                                                      Just (RemoteBranch remote branchName) -> assertFailure $ "expected no remote branch but got: " <> remote <> "/" <> branchName
-                                                      Nothing                        -> pure ()
-                                                  ) parseResult
-  in testCase "testWithoutRemoteBranch" result
-
+unit_parseWithoutRemoteBranch :: Assertion
+unit_parseWithoutRemoteBranch = 
+  let parseResult = parse remoteBranch "" "* master b93b0b7 More WIP" in
+  either (assertFailure . show) (\rBranch -> 
+                                    case rBranch of 
+                                      Just (RemoteBranch remote branchName) -> assertFailure $ "expected no remote branch but got: " <> remote <> "/" <> branchName
+                                      Nothing                               -> pure ()
+                                ) parseResult
