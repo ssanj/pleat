c11eef3067a6511efd9f1cd4f82c05451525be91
sanjiv sahayam <sanjsmailbox@gmail.com>
2020-06-29 09:05:44 +1000

    WIP: Refactor

--- src/Config/Model.hs
+++ src/Config/Model.hs
@@ -15,19 +15,23 @@ module Config.Model
         ,  PromptSeparator(..)
         ) where
 
--- import qualified Parser.GitParser as GP
-
 newtype HostnameOption  = HostnameOption { _overrideHostname ::  Maybe Hostname } deriving stock (Eq, Show)
+
 newtype Hostname        = Hostname { _hostname :: String } deriving stock (Eq, Show)
+
 newtype Prompt          = Prompt { _prompt :: String } deriving stock (Eq, Show)
+
 newtype PromptSeparator = PromptSeparator { _promptSeparator :: String } deriving stock (Eq, Show)
+
 newtype MaxPathLength   = MaxPathLength { _pathLength :: Int } deriving stock (Eq, Show)
+
 newtype PathOption      = PathOption { _maxPathLength :: MaxPathLength } deriving stock (Eq, Show)
 
 data PleatOption a   = OptionOff | OptionOn a deriving stock (Eq, Show)
-data GitOption       = GitOption deriving stock (Eq, Show)
-data TimestampOption = TimestampOption deriving stock (Eq, Show)
 
+data GitOption       = GitOption deriving stock (Eq, Show)
+
+data TimestampOption = TimestampOption deriving stock (Eq, Show)
 
 data Config = Config {
    _pleatHostnameOverrideOption :: PleatOption HostnameOption
